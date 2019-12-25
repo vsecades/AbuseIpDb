ef9b43878ffcffb6ed18227ef07702216cf416bd
Sebastian Bonhag <sebastian@bonhag.de>
2019-12-25 16:12:17 +0100

    Increasing version and release number

--- abuseipdb/__init__.py
+++ abuseipdb/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '1.3.0'  #: the working version
-__release__ = '1.0.0'  #: the release version
+__version__ = '2.0.0'  #: the working version
+__release__ = '2.0.0'  #: the release version
 
 from abuseipdb.api_v1 import check_cidr, check_ip, configure_api_key, report_ip
