776498534e8a98b1cc0fbdaadd04449f803fa3b6
Sebastian Bonhag <sebastian@bonhag.de>
2019-12-26 01:43:39 +0100

    Test current behavior for successful calls

--- tests/test_legacy.py
+++ tests/test_legacy.py
@@ -26,12 +26,35 @@ class LegacyTestCase(TestCase):
         with self.assertRaises(KeyError):
             abuseipdb.check_ip(ip='192.0.2.123')
 
+    @patch('unirest.get')
+    def test_check_ip(self, mock):
+        self.reset_configuration()
+        abuseipdb.configure_api_key('some_API_key')
+        abuseipdb.check_ip(ip='192.0.2.123')
+        mock.assert_called_once_with('https://www.abuseipdb.com/check/192.0.2.123/json?key=some_API_key&days=30')
+
     def test_check_cidr__no_api_key_configured(self):
         self.reset_configuration()
         with self.assertRaises(KeyError):
             abuseipdb.check_cidr(cidr='192.0.2.123')
 
+    @patch('unirest.get')
+    def test_check_cidr(self, mock):
+        self.reset_configuration()
+        abuseipdb.configure_api_key('some_API_key')
+        abuseipdb.check_cidr(cidr='192.0.2.0/24')
+        # This URL looks wrong, but the APIv1 documentation doesn't indicate,
+        # that the / needs to be encoded.
+        mock.assert_called_once_with('https://www.abuseipdb.com/check-block/json?key=some_API_key&network=192.0.2.0/24&days=30')
+
     def test_report_ip__no_api_key_configured(self):
         self.reset_configuration()
         with self.assertRaises(KeyError):
             abuseipdb.report_ip(ip='192.0.2.123', categories="22")
+
+    @patch('unirest.get')
+    def test_report_ip(self, mock):
+        self.reset_configuration()
+        abuseipdb.configure_api_key('some_API_key')
+        abuseipdb.report_ip(ip='192.0.2.123', categories="22")
+        mock.assert_called_once_with('https://www.abuseipdb.com/report/json?key=some_API_key&category=22&comment=&ip=192.0.2.123')
