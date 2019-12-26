bbed4a5670241913d4338edcafbb139c2a024d1f
Sebastian Bonhag <sebastian@bonhag.de>
2019-12-26 01:09:28 +0100

    Test the setting of the API key

--- tests/test_legacy.py
+++ tests/test_legacy.py
@@ -1,7 +1,22 @@
 from unittest import TestCase
 
+import abuseipdb
+from abuseipdb.parameters import Parameters
+from mock import patch
+
 
 class LegacyTestCase(TestCase):
 
-    def test_tox_configuration_works(self):
-        pass
+    def reset_configuration(self):
+        # This is required to test the configuration isolated
+        Parameters.configuration = {}
+
+    def test_configure_api_key__key_provided(self):
+        self.reset_configuration()
+        abuseipdb.configure_api_key('some API key')
+        assert Parameters.get_config() == {'API_KEY': 'some API key'}
+
+    def test_configure_api_key__no_key_provided(self):
+        self.reset_configuration()
+        abuseipdb.configure_api_key(None)
+        assert Parameters.get_config() == {}
